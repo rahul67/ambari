dc41bb358b50180eee8880941bfc8ce6b1c2d07f
rahul agarwal <rahul@ragarwal.me>
2015-02-20 01:22:54 +0530

    added Tez-UI as service component.

--- ambari-server/src/main/java/org/apache/ambari/server/Role.java
+++ ambari-server/src/main/java/org/apache/ambari/server/Role.java
@@ -109,6 +109,7 @@ public class Role {
   public static final Role KERBEROS_SERVICE_CHECK = valueOf("KERBEROS_SERVICE_CHECK");
   public static final Role METRIC_COLLECTOR = valueOf("METRIC_COLLECTOR");
   public static final Role METRIC_MONITOR = valueOf("METRIC_MONITOR");
+    public static final Role TEZ_UI = valueOf("TEZ_UI");
 
   private String name = null;
   

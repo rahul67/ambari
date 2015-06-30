bd1ca3d865cda4f2dc14f34f1546731b0d0d835d
rahul agarwal <rahul@ragarwal.me>
2015-06-30 20:19:20 +0530

    Cosmos Integration

---
+++ ambari-server/src/main/resources/common-services/COSMOS/1.7.0/package/scripts/cosmos_collectd.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+"""
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
+
+from resource_management import *
+from resource_management.libraries.functions.security_commons import build_expectations, \
+   get_params_from_filesystem, validate_security_config_properties, \
+  FILE_TYPE_XML
+from cosmos import cosmos
+from cosmos_service import cosmos_service
+from status import check_service_status
+
+class CosmosCollectd(Script):
+  def install(self, env):
+    self.install_packages(env)
+    # self.configure(env) # for security
+
+
+  def configure(self, env):
+    import params
+    env.set_params(params)
+    cosmos(name='collectd')
+
+  def start(self, env):
+    self.configure(env) # for security
+
+    cosmos_service( 'collectd',
+                   action = 'start'
+    )
+
+  def stop(self, env):
+    import params
+    env.set_params(params)
+
+    # Sometimes, stop() may be called before start(), in case restart() is initiated right after installation
+    self.configure(env) # for security
+
+    cosmos_service( 'collectd', action = 'stop')
+
+  def status(self, env):
+    import status_params
+    env.set_params(status_params)
+    check_service_status(name='collectd')
+
+if __name__ == "__main__":
+  CosmosCollectd().execute()
